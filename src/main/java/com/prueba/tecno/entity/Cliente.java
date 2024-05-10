22a628085bc861270626a64f9f898d6223b1dc42
Carlos Barrera Aleman <115600193+devpcarlos@users.noreply.github.com>
2024-05-10 10:14:32 -0500

    se crea la entidad cliente

--- src/main/java/com/prueba/tecno/entity/Cliente.java
+++ src/main/java/com/prueba/tecno/entity/Cliente.java
@@ -1,28 +1,39 @@
 package com.prueba.tecno.entity;
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDateTime;
 import java.util.List;
 
 @Entity(name = "cliente")
-@Getter
-@Setter
+@EntityListeners(AuditingEntityListener.class)
 @AllArgsConstructor
 @NoArgsConstructor
+@Data
 public class Cliente {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    private String name;
+    private String nombre;
     private String apellido;
-    private String direccion;
-    private String ciudad;
-    private LocalDateTime craeted;
+    private String celular;
+    private String correo;
+    private String password;
+    private LocalDateTime created;
     private LocalDateTime updated;
+    @PrePersist
+    public void prePersist() {
+        this.created = LocalDateTime.now();
+        this.updated = LocalDateTime.now();
+    }
+
+    @PreUpdate
+    public void preUpdate() {
+        this.updated = LocalDateTime.now();
+    }
 }
