9fca15cdbc5b33cf68af4ada4dff8116c6c413ca
Carlos Barrera Aleman <115600193+devpcarlos@users.noreply.github.com>
2024-05-10 10:15:25 -0500

    se crea el repositorio de cliente y metodos

--- src/main/java/com/prueba/tecno/repository/ClienteRepository.java
+++ src/main/java/com/prueba/tecno/repository/ClienteRepository.java
@@ -2,6 +2,22 @@ package com.prueba.tecno.repository;
 
 import com.prueba.tecno.entity.Cliente;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface ClienteRepository extends JpaRepository<Cliente, Long> {
+
+    @Query(value = "SELECT crypt(?1, gen_salt('bf'))", nativeQuery = true)
+    String encryptPassword(String password);
+
+    Cliente findByCorreo(String correo);
+
+    @Query(value = "SELECT * " + "FROM Cliente c " +
+            "WHERE c.correo = :correo " +
+            "AND c.password = crypt(:password, c.password)",
+            nativeQuery = true)
+    Cliente findByEmailToLogin(@Param("correo") String correo,
+                               @Param("password") String password);
 }
