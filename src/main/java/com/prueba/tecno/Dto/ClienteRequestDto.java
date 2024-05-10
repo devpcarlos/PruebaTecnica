749156ae3abbc03f506e3127df5ceb086710db16
Carlos Barrera Aleman <115600193+devpcarlos@users.noreply.github.com>
2024-05-10 10:13:43 -0500

    se crea el resquest del cliente

--- src/main/java/com/prueba/tecno/Dto/ClienteRequestDto.java
+++ src/main/java/com/prueba/tecno/Dto/ClienteRequestDto.java
@@ -1,14 +1,28 @@
 package com.prueba.tecno.Dto;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
 
 @Data
-public class ClienteDto {
-    private String name;
+@AllArgsConstructor
+@NoArgsConstructor
+public class ClienteRequestDto {
+
+    @NotBlank(message = "El campo nombre no puede estar vacio")
+    private String nombre;
+    @NotBlank(message = "El campo apellido no puede estar vacio")
     private String apellido;
-    private String direccion;
-    private String ciudad;
-    private LocalDateTime updated;
+    @NotBlank(message = "El campo celular no puede estar vacio")
+    private String celular;
+    @Email(message = "Debe ser un correo valido")
+    private String correo;
+    @NotBlank(message = "El campo contraseña no puede estar vacio")
+    @Size(min = 8,max = 255, message = "La contraseña debe tener un minimo de 8 y un maximo de 20")
+    private String password;
 }
