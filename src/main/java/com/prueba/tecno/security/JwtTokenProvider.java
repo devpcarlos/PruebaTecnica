6633d3e948082823650ce706fd8ac8016f4cfde8
Carlos Barrera Aleman <115600193+devpcarlos@users.noreply.github.com>
2024-05-13 16:28:56 -0500

    implementando excepciones

--- src/main/java/com/prueba/tecno/security/JwtTokenProvider.java
+++ src/main/java/com/prueba/tecno/security/JwtTokenProvider.java
@@ -1,6 +1,7 @@
 package com.prueba.tecno.security;
 
 import com.prueba.tecno.exceptions.ExceptionHelper;
+import com.prueba.tecno.exceptions.Exceptions;
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.ExpiredJwtException;
 import io.jsonwebtoken.Jwts;
@@ -19,10 +20,10 @@ public class JwtTokenProvider {
     @Value("${jwt.secret}")
     private String jwtSecret;
     @Value("${jwt.expiration}")
-    private  int jwtExpiration;
+    private int jwtExpiration;
 
-    private SecretKey getSecretKey(){
-        byte[] decodeKey = Base64.getDecoder().decode(jwtSecret.getBytes(StandardCharsets.UTF_8));
+    private SecretKey getSecretKey() {
+        byte[] decodeKey = Base64.getDecoder().decode(jwtSecret.getBytes(StandardCharsets.UTF_8));git 
         return Keys.hmacShaKeyFor(decodeKey);
     }
 
@@ -30,39 +31,40 @@ public class JwtTokenProvider {
                                 final Long userId,
                                 final String name) {
         try {
-        //Generar UUID
-        String uuid = UUID.randomUUID().toString();
+            //Generar UUID
+            String uuid = UUID.randomUUID().toString();
 
-        Date now = new Date();
-        Date expiryDate = new Date(now.getTime() + jwtExpiration * 1000L);
-        // Agregar reclamaciones privadas
-        String token = Jwts.builder()
-                .subject(uuid)
-                .claim("Id", userId)
-                .claim("email", email)
-                .claim("nombre", name)
-                .issuedAt(new Date())
-                .expiration(expiryDate)
-                .signWith(getSecretKey()).compact();
+            Date now = new Date();
+            Date expiryDate = new Date(now.getTime() + jwtExpiration * 1000L);
+            // Agregar reclamaciones privadas
+            String token = Jwts.builder()
+                    .subject(uuid)
+                    .claim("Id", userId)
+                    .claim("email", email)
+                    .claim("nombre", name)
+                    .issuedAt(new Date())
+                    .expiration(expiryDate)
+                    .signWith(getSecretKey()).compact();
 
-        return token;
+            return token;
         } catch (ExpiredJwtException e) {
             throw ExceptionHelper.unauthorized("El token JWT ha expirado");
         }
     }
 
-    public Claims decode(String token) throws Exception {
-
+    public Claims decode(String token) throws Exceptions {
         try {
             return Jwts.parser()
                     .verifyWith(getSecretKey())
                     .build()
                     .parseSignedClaims(token)
                     .getPayload();
-        }catch (ExpiredJwtException ex){
-            throw ExceptionHelper.unauthorized("El token JWT no ha expirado");
+        } catch (ExpiredJwtException e) {
+            throw ExceptionHelper.unauthorized("El token JWT ha expirado");
         }
 
     }
 
 }
+
+
