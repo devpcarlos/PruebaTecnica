fe692c3933bed5a8522fdc0abdc1511c2c6b7e2c
Carlos Barrera Aleman <115600193+devpcarlos@users.noreply.github.com>
2024-05-13 16:26:06 -0500

    creando consultas

--- src/main/java/com/prueba/tecno/repository/VehiculoRepository.java
+++ src/main/java/com/prueba/tecno/repository/VehiculoRepository.java
@@ -1,9 +1,20 @@
 package com.prueba.tecno.repository;
 
 import com.prueba.tecno.entity.Vehiculo;
+import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface VehiculoRepository  extends JpaRepository<Vehiculo, Long> {
+    @Query("SELECT v FROM vehiculo v WHERE v.cliente.id = :userId AND v.matricula = :matricula")
+    Vehiculo findByMatriculaAndId(String matricula, Long userId);
 
-    Vehiculo findByMatricula(String matricula);git 
+    @Query("SELECT v FROM vehiculo v WHERE v.cliente.id = :userId")
+    List<Vehiculo> findAllByUserId(Long userId);
+
+    @Transactional
+    Long deleteByIdAndClienteId(Long id, Long clienteId);
+
 }
